d76716688cf4ba1eecae08b275088c00f78401ca
zforler <1517323360@qq.com>
2018-07-11 10:25:46 +0800

    20180711

--- js/index.js
+++ js/index.js
@@ -6,15 +6,16 @@ $(function(){
  * 根据主题图片设置主题颜色
  */
 function setThemeColor(){
-    let src = 'img/b_2.jpg';
+    let src = 'img/bg_3.jpg';
     let colorThief = new ColorThief();
     let img = document.createElement('img');
     img.src = src;
-    img.width = '1000';
-    img.height = '1000';
+    img.width = 1000;
+    img.height = 1000;
     img.onload = function(){
         let color = colorThief.getColor(img);
         color = color.join(',');
+        console.log(colorThief.getPalette(img,3,255));
         document.body.style.setProperty('--theme-color',  `rgb(${color})`);
         document.body.style.setProperty('--bgimg',  `url(../${src})`);
     };
